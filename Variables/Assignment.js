6d327486464208b0dd0d420aa477baddcee8a8ed
lemi5 <chisom.gbulie@pau.edu.ng>
2023-04-17 09:59:02 +0100

    My first commit

---
+++ Variables/Assignment.js
@@ -0,0 +1,11 @@
+let name;
+let adjective;
+let noun;
+
+name = "Michael";
+adjective = "tired";
+noun = "Patrick's";
+
+console.log("Please excuse " + name);
+console.log("Who was far too " + adjective);
+console.log("to attend " + noun + " class.");
---
+++ Variables/index.js
@@ -0,0 +1,7 @@
+let name;
+let age;
+name = "Michael";
+age = 20;
+
+console.log("My name is "+name);
+console.log("My age is "+age);
